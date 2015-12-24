ffb40433bb093f1470f9852be8e424d94707748a
Ethan Buchman <ethan@coinculture.info>
2015-12-23 20:34:13 -0500

    TMLOG and TMSYNC in docker image

--- DOCKER/run.sh
+++ DOCKER/run.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+if [[ "$TMLOG" == "" ]]; then
+	TMLOG="notice"
+fi
+
+if [[ "$TMSYNC" == "" ]]; then
+	TMSYNC=false
+fi
+
 mkdir -p $GOPATH/src/$TMREPO
 cd $GOPATH/src/$TMREPO
 git clone https://$TMREPO.git .
@@ -7,4 +15,4 @@ git fetch
 git reset --hard $TMHEAD
 go get -d $TMREPO/cmd/tendermint
 make
-tendermint node --seeds="$TMSEEDS" --moniker="$TMNAME"
+tendermint node --seeds="$TMSEEDS" --moniker="$TMNAME" --log_level="$TMLOG" --fast_sync="$TMSYNC"
